187a9b5569649d5a8d7bc14ed197eed0270345c9
zakimal <yoichiozaki@microsoft.com>
2022-04-21 18:09:41 +0900

    validate strings

---
+++ String/ValidateStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Linq;
+
+static Boolean IsUpperCase(string s)
+{
+    return s.All(char.IsUpper);
+}
+
+static Boolean IsLowerCase(string s)
+{
+    return s.All(char.IsLower);
+}
+
+static Boolean IsPasswordComplex(string s)
+{
+    return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+}
+
+Console.WriteLine(IsUpperCase("HELLO"));
+Console.WriteLine(IsLowerCase("hello"));
+Console.WriteLine(IsPasswordComplex("ThisIsC0mplex!"));
