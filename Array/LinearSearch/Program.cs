02ed0fc352be1869756bdbd8f557ce9d9b269fe8
zakimal <yoichiozaki@microsoft.com>
2022-04-21 19:07:34 +0900

    linear search

---
+++ Array/LinearSearch/Program.cs
@@ -0,0 +1,16 @@
+﻿static Boolean LinearSearch(int[] arr, int target)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(LinearSearch(arr, 5));
+Console.WriteLine(Array.Find(arr, ele => ele == 3));
+Array.FindAll(arr, ele => ele >= 5);
