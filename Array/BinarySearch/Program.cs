419bfea25b248b8c90fa10e3eab5f8b8a12f5c61
zakimal <yoichiozaki@microsoft.com>
2022-04-21 19:16:24 +0900

    binary search

---
+++ Array/BinarySearch/Program.cs
@@ -0,0 +1,25 @@
+﻿static int BinarySearch(int[] arr, int target)
+{
+    bool isOk(int mid)
+    {
+        return arr[mid] < target;
+    }
+    int ok = -1;
+    int ng = arr.Length;
+    while (Math.Abs(ok - ng) > 1)
+    {
+        int mid = (ok + ng) / 2;
+        if (isOk(mid))
+        {
+            ok = mid;
+        }
+        else
+        {
+            ng = mid;
+        }
+    }
+    return ok;
+}
+
+int[] arr = { 1, 2, 3, 4, 5, 6 };
+Console.WriteLine(BinarySearch(arr, 4));
