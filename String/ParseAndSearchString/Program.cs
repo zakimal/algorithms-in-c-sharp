b85ffbbd3d59b1545d11f68d9805d62ee812b75f
zakimal <yoichiozaki@microsoft.com>
2022-04-21 18:35:32 +0900

    parse and search string

--- String/ParseAndSearchString/Program.cs
+++ String/ParseAndSearchString/Program.cs
@@ -1,12 +1,35 @@
-﻿static void ParseContents(string s) {
-    foreach(char ch in s) {
+﻿static void ParseContents(string s)
+{
+    foreach (char ch in s)
+    {
         Console.WriteLine(ch);
     }
 
     Console.WriteLine();
-    for (int i = 0; i < s.Length; i++) {
+    for (int i = 0; i < s.Length; i++)
+    {
         Console.WriteLine(s[i]);
     }
 }
 
+static Boolean IsAtEvenIndex(string s, char ch)
+{
+    if (String.IsNullOrEmpty(s))
+    {
+        return false;
+    }
+    for (int i = 0; i < s.Length / 2 + 1; i = +2)
+    {
+        if (s[i] == ch)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 ParseContents("Hello World");
+
+Console.WriteLine(IsAtEvenIndex("HeLLo", 'L'));
+
+Console.WriteLine(IsAtEvenIndex("", 's'));
