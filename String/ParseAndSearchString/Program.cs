509fcf5e05f48bf808286be141f59e92c367fd5e
zakimal <yoichiozaki@microsoft.com>
2022-04-21 18:26:45 +0900

    parse and search strings

---
+++ String/ParseAndSearchString/Program.cs
@@ -0,0 +1,12 @@
+﻿static void ParseContents(string s) {
+    foreach(char ch in s) {
+        Console.WriteLine(ch);
+    }
+
+    Console.WriteLine();
+    for (int i = 0; i < s.Length; i++) {
+        Console.WriteLine(s[i]);
+    }
+}
+
+ParseContents("Hello World");
