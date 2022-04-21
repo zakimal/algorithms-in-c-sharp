df1ee3e82e191bac39767759d1230d67fa31bae9
zakimal <yoichiozaki@microsoft.com>
2022-04-21 18:45:48 +0900

    create algorithm-driven strings

---
+++ String/CreateAlgorithmDrivenStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+static string Reverse(string s)
+{
+    if (String.IsNullOrEmpty(s))
+    {
+        return s;
+    }
+
+    StringBuilder reversed = new StringBuilder(s.Length);
+    for (int i = s.Length - 1; 0 <= i; i--)
+    {
+        reversed.Append(s[i]);
+    }
+
+    return reversed.ToString();
+}
+
+static string Reverse2(string s)
+{
+    if (String.IsNullOrEmpty(s))
+    {
+        return s;
+    }
+    char[] arr = s.ToCharArray();
+    Array.Reverse(arr);
+    return new String(arr);
+}
+
+Console.WriteLine(Reverse("Hello World"));
+Console.WriteLine(Reverse2("Hello World"));
